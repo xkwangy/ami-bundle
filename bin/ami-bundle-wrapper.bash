942a2b14c528712ad172996bd18f9c6d22cd063c
Ian Ward <ianshward@gmail.com>
2013-12-17 18:45:34 -0500

    Make it possible to call just signal or just bundle

--- bin/ami-bundle-wrapper.bash
+++ bin/ami-bundle-wrapper.bash
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+# Either signal or bundle
+OP=$1
 # Name to give the AMI
-NAME=$1
-# Bucket prefix.  Region is automatically appended. If foo is the provided
-# prefix then the image will be stored in foo-us-east-1 if bundling in us-east-1
-PREFIX=$2
+NAME=$2
+# AMI will be uploaded to a bucket called $PREFIX-$REGION
+PREFIX=$3
 # Comma separated list of files to exclude
-EXCLUDE=$3
+EXCLUDE=$4
 # Comma separated list of files to include
-INCLUDE=$4
-AMIKEY=$5
-AMICRT=$6
+INCLUDE=$5
+AMIKEY=$6
+AMICRT=$7
 # Path to CloudFormation wait condition handle
-HANDLE=$7
+HANDLE=$8
 
 REGION=$(wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone | sed '$s/.$//')
 USER=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep accountId | awk -F\" '{print $4}')
@@ -32,31 +33,39 @@ function signal() {
     fi
 }
 
-# Do not bundle if an AMI already exists.
-# TODO Also check S3 because image parts are uploaded to S3 before the AMI
-# is registered.
-aws --region=$REGION ec2 describe-images --owners "self" |grep $NAME
-if [ $? -eq 0 ]; then
-    signal SUCCESS "Requested AMI already exists in S3 bucket"
-fi
-
-timeout 1200 /usr/local/bin/ami-bundle.bash \
-    -r $REGION \
-    -b $PREFIX \
-    -n $NAME \
-    -u $USER \
-    -k $AMIKEY \
-    -c $AMICRT \
-    -e "$EXCLUDE" \
-    -i "$INCLUDE" 2> /tmp/ami-bundle-output
-
-if [ $? -eq 0 ]; then
-    signal SUCCESS "ami-bundle.bash succeeded"
-else
-    if [[ -s /tmp/ami-bundle-output ]]; then
-        ERROR=$(cat /tmp/ami-bundle-output)
-    else
-        ERROR="timed out"
+function bundle() {
+    # Do not bundle if an AMI already exists.
+    # TODO Also check S3 because image parts are uploaded to S3 before the AMI
+    # is registered.
+    aws --region=$REGION ec2 describe-images --owners "self" |grep $NAME
+    if [ $? -eq 0 ]; then
+        signal SUCCESS "Requested AMI already exists in S3 bucket"
     fi
-    signal FAILURE "ami-bundle.bash error: $ERROR"
+
+    timeout 1200 /usr/local/bin/ami-bundle.bash \
+        -r $REGION \
+        -b $PREFIX \
+        -n $NAME \
+        -u $USER \
+        -k $AMIKEY \
+        -c $AMICRT \
+        -e "$EXCLUDE" \
+        -i "$INCLUDE" 2> /tmp/ami-bundle-output
+
+    if [ $? -eq 0 ]; then
+        signal SUCCESS "ami-bundle.bash succeeded"
+    else
+        if [[ -s /tmp/ami-bundle-output ]]; then
+            ERROR=$(cat /tmp/ami-bundle-output)
+        else
+            ERROR="timed out"
+        fi
+        signal FAILURE "ami-bundle.bash error: $ERROR"
+    fi
+}
+
+if [ "$OP" == "signal" ]; then
+    signal
+elif [ "$OP" == "bundle" ]; then
+    bundle
 fi
